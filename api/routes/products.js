92fbf98f18ee2a089797860da19df488560499f3
completejavascript <completejavascript.super@gmail.com>
2019-01-08 00:27:06 +0700

    Add bodyParser, setup CORS

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,14 +8,20 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
+  
   res.status(201).json({
-    message: 'Handling POST requests to /products'
+    message: 'Handling POST requests to /products',
+    createdProduct: product
   });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  
+
   if (id === 'special') {
     res.status(200).json({
       message: 'You discovered the special ID',
