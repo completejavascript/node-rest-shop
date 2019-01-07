6b7f3df55ea2f325adccf5ba59d7419e1f9ad800
completejavascript <completejavascript.super@gmail.com>
2019-01-07 23:43:15 +0700

    Add more routes

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-
 const app = express();
 
-/**
- * Middleware
- */
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
-  });
-});
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders'); 
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
