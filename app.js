a3ecde3a2eabf42f100295b1774867042b2d5852
completejavascript <completejavascript.super@gmail.com>
2019-01-09 00:15:04 +0700

    Add moongoose and product database function

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+  `mongodb+srv://lampham:${process.env.MONGO_ATLAS_PW}@node-rest-api-utlml.mongodb.net/test?retryWrites=true`,
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
